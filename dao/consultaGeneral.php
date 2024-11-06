0c40edac5e6802250c68bf8a7c7d05e3d49888a7
hadbet <hadbet.altamirano@grammer.com>
2024-11-06 16:06:33 -0600

    Carga Inicial del Proyecto

--- dao/consultaGeneral.php
+++ dao/consultaGeneral.php
@@ -1,15 +1,27 @@
 <?php
 
 include_once('db/db_Freight.php');
+$filtro = $_GET['filtro'];
 
-ContadorApu();
+ContadorApu($filtro);
 
-function ContadorApu()
+function ContadorApu($filtro)
 {
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT `RECOVERY`, SUM(`Q_COST`) AS SUMA,`MONEDA` FROM `PREMIUM` WHERE `MONEDA` = 'USD' and ESTATUS = 'C' GROUP BY `RECOVERY`, `MONEDA` UNION ALL SELECT `RECOVERY`, SUM(`Q_COST`) AS SUMA,`MONEDA` FROM `PREMIUM` WHERE `MONEDA` = 'MXN' and ESTATUS = 'C' GROUP BY `RECOVERY`, `MONEDA`;");
+    $bandera = "1=1";
+
+    if ($filtro == 'PO45'){
+        $bandera = "REFERENCE like '45%'";
+    }
+    if ($filtro == 'OTROS'){
+        $bandera = "not REFERENCE like '45%'";
+    }
+
+    $datos = mysqli_query($conex, "SELECT RECOVERY, SUM(Q_COST) AS SUMA,MONEDA FROM PREMIUM WHERE MONEDA = 'USD' and ESTATUS = 'C' and  $bandera GROUP BY RECOVERY, MONEDA 
+                                        UNION ALL 
+                                        SELECT RECOVERY, SUM(Q_COST) AS SUMA,MONEDA FROM PREMIUM WHERE MONEDA = 'MXN' and ESTATUS = 'C' and  $bandera GROUP BY RECOVERY, MONEDA;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
